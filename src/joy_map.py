f8128ed48e370285305dce066bc15d5c40f5e8d4
AyumiizZ <Korrawitc@gmail.com>
2019-06-22 19:51:43 +0700

    :sparkles: add voltage checker

--- src/joy_map.py
+++ src/joy_map.py
@@ -2,14 +2,20 @@
 import rospy
 import os
 from sensor_msgs.msg import Joy
-from zeabus_utility.msg import ControlCommand
-from zeabus_utility.srv import SendControlCommand
+from zeabus_utility.msg import ControlCommand, TelemetryStruct
+from zeabus_utility.srv import SendControlCommand, Telemetry
 from teleop_lib import JoyTools, Convert
 from time import sleep
 
 
 convert = Convert()
 
+
+def calculate_voltage(data):
+    sum_voltage = 0
+    for i in data:
+        sum_voltage += i.voltage
+    return sum_voltage/len(data)
 
 def break_message(header, seq):
     msg = ControlCommand()
@@ -47,15 +53,19 @@ def message(header, seq=0, x=None, y=None, z=None, yaw=None, reset=False):
 
 def run():
     pub = rospy.Publisher('/control/thruster', ControlCommand)
+    bat_check_call = rospy.ServiceProxy('/hardware/thruster_feedback', Telemetry)
     # call = rospy.ServiceProxy('/control/thruster', SendControlCommand)
-    print("fuck")
     locked = True
+    not_break_rule = True
+    last_battery = None
     default_z = 0
     i = 1
     while not rospy.is_shutdown():
         os.system('clear')
         print('default_z: ',default_z)
-        if(joy.RT_PRESS):
+        print('break_rule', not_break_rule)
+        print('Last check battery', last_battery)
+        if(joy.RT_PRESS and not_break_rule):
             msg = message(header=joy.msg.header,
                           seq=i,
                           x=convert.to_x(joy.buttons.stick.left.y),
@@ -69,22 +79,32 @@ def run():
             else:
                 print('Waiting')
             locked = False
+        elif(joy.buttons.power == joy.press):
+            print('checking battery')
+            response = bat_check_call()
+            last_battery = calculate_voltage(response.telemetry)
+        elif(joy.buttons.LB == joy.press and joy.buttons.start == joy.press):
+            not_break_rule = True
+        elif(joy.buttons.RB == joy.press and joy.buttons.start == joy.press):
+            not_break_rule = False
+        elif(not_break_rule == False):
+            msg = break_message(header=joy.msg.header, seq=i)
+            pub.publish(msg)
+            print(msg)
+            i += 1
         elif(joy.buttons.LB == joy.press and joy.buttons.back == joy.press):
             break
         elif(joy.buttons.LB == joy.press and joy.buttons.Y == joy.press):
             default_z = 0
         elif(joy.buttons.RB == joy.press and joy.buttons.Y == joy.press):
             default_z = convert.DEFAULT_Z
-        elif(joy.buttons.RB == joy.press and joy.buttons.start == joy.press):
-            msg = break_message(header=joy.msg.header, seq=i)
-            pub.publish(msg)
-            print(msg)
-            i += 1
         else:
             print('Press RT to control')
             print('Press RB+Y to set default_z = ' + str(convert.DEFAULT_Z))
             print('Press LB+Y to set default_z = 0')
-            print('Press RB+START to force stop AUV')
+            print('Press RB+START to disable force stop AUV')
+            print('Press LB+START to enable force stop AUV')
+            print('Press power to check battery (F310 only)')
             if default_z != 0:
                 pub.publish(message(header=joy.msg.header, seq=i,z=default_z))
                 i+=1
